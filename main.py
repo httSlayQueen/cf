ab9bdf364eecf806db97267ee82d8daed4e3574e
httSlayQueen <1924226520@qq.com>
2026-05-11 12:49:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1140,7 +1140,6 @@ def main():
             lat_sec = latency_map.get(node, float('inf'))
             if lat_sec != float('inf'):
                 print(f"{i}. {node} 速度 {speed:.2f} Mbps 延迟 {lat_sec*1000:.2f} ms")
-                final_selected[i-1] = node + f" [{speed:.2f} Mbps|{lat_sec*1000:.2f} ms]"
                 final_selected[i-1] = node + f" [{speed:.2f} Mbps|{lat_sec*1000:.2f} ms]"  #在行末加入测速结果
             else:
                 print(f"{i}. {node} 速度 {speed:.2f} Mbps")
